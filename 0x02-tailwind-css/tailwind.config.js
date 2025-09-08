e2898a590ff127901b4d50e926517550c1d14b47
Orolamilekan <Orolamilekan@gmail.com>
2025-09-08 05:58:51 +0100

    Updating Tailwind css

--- 0x02-tailwind-css/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.html"],   // scan ALL html files inside src
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
